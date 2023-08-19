da01fc0b92c840616d79055d5201e990ba59f71c
Noushadozi <noushadozi333@gmail.com>
2023-08-19 14:17:20 +0600

    calculation done by functional method

--- scripts/functional_area.js
+++ scripts/functional_area.js
@@ -11,12 +11,27 @@ function calculateRhombusArea() {
   console.log(rhombusArea);
 }
 function calculatePentagonArea() {
-    const pentagonPField = document.getElementById("pentagon-p");
-    const pentagonBField = document.getElementById("pentagon-b");
-    const pentagonPString = pentagonPField.value;
-    const pentagonBString = pentagonBField.value;
-    const pentagonP = parseFloat(pentagonPString);
-    const pentagonB = parseFloat(pentagonBString);
-    const pentagonArea = 0.5 * pentagonP * pentagonB;
-    console.log(pentagonArea);
+  const pentagonP = getValueById("pentagon-p");
+  const pentagonB = getValueById("pentagon-b");
+  const pentagonArea = 0.5 * pentagonP * pentagonB;
+  console.log(pentagonArea);
+}
+
+function calculateRectangleArea() {
+  const rectangleW = getValueById("rectangle-w");
+  const rectangleL = getValueById("rectangle-l");
+  const rectangleArea = rectangleW * rectangleL;
+  console.log(rectangleArea);
+}
+function calculateParallelogramArea() {
+  const parallelogramB = getValueById("parallelogram-b");
+  const parallelogramH = getValueById("parallelogram-h");
+  const parallelogramArea = parallelogramB * parallelogramH;
+  console.log(parallelogramArea);
+}
+function calculateEllipseArea() {
+    const ellipseA = getValueById("ellipse-a");
+    const ellipseB = getValueById("ellipse-b");
+    const ellipseArea = 3.1416 * ellipseA * ellipseB;
+    console.log((ellipseArea).toFixed(2));
   }
